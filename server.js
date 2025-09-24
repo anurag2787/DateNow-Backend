9d383b306b569fbacd14811d795600bd2de0e25b
Anurag Yadav <143180737+anurag2787@users.noreply.github.com>
2025-09-24 14:54:43 +0530

    Refactor MongoDB connection string usage

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.get("/", (req, res) => {
   app.use('/verify',verifyEmail);
 
   mongoose
-  .connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
